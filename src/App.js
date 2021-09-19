0b9497a1443340e2fa3cf64a01fb48aefe2a4b3e
Unmoy <vampireklug@gmail.com>
2021-09-19 16:36:06 +0600

    fixed folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header/Header';
+import "./App.css";
+import Navbar from "./components/Home/Navbar/Navbar";
+import Header from "./components/Home/Header/Header";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Header/>
-      <h3>This is Sixty Ninth streets home page....</h3>
+      <Navbar />
+      <Header />
+      <h3>This is Sixty Ninth Streets Home Page....</h3>
     </div>
   );
 }
