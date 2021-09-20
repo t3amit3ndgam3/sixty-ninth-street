392cf020d93e456501be95b93043cb8c4dee1d9a
niazmurshedsiam <niazmurshed16@gmail.com>
2021-09-20 12:25:35 +0600

    part how to work and property side work done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Header from "./Header/Header";
+import Work from "./Work/Work";
+import Properties from "./Properties/Properties";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <Work></Work>
+      <Properties></Properties>
     </div>
   );
 };
---
+++ src/components/Home/Work/Work.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Property from "../../../images/Property.png";
+import BuyProperty from "../../../images/BuyProperty.png"
+import MakeInvestment from "../../../images/MakeInvestment.png";
+import WorkDetails from "../WorkDetails/WorkDetails";
+const Work = () => {
+  const worksData = [
+    {
+      name: "Find Property.",
+      image: Property,
+      description: "",
+    },
+    {
+      name: "Buy Property.",
+      image: BuyProperty,
+      description: "",
+    },
+    {
+      name: "Make Investment.",
+      image: MakeInvestment,
+      description: "",
+    },
+  ];
+  return (
+    <section className="services-container mt-5">
+      <div className="text-center">
+        <h1 className="text-warning">How It Works</h1>
+      </div>
+      <div className="d-flex justify-content-center">
+        <div className="w-75 row mt-5 pt-5">
+          {worksData.map((Work) => (
+            <WorkDetails Work={Work}></WorkDetails>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Work;
