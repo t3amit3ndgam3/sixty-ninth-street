7c9d9419b597f08d8a104983c3b554a975630a91
GolamRabby96 <golamrabbynwu@gmail.com>
2021-09-21 13:02:42 +0600

    dream section done .. only  number randerning need to fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Header from "./Header/Header";
+import DreamHome from "./DreamHome/DreamHome";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <DreamHome/>
     </div>
   );
 };
