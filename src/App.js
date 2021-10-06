69aa22e96ede2cb60d003b27b91636432540077c
GolamRabby96 <golamrabbynwu@gmail.com>
2021-10-06 21:28:43 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,34 +1,5 @@
 import "./App.css";
 import Home from "./components/Home/Home";
-<<<<<<< HEAD
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Navbar from "./components/Home/Navbar/Navbar";
-import PropertiesList from "./components/PropertiesList/PropertiesList";
-import TEST from "./components/TEST.js";
-
-
-function App() {
-  return (
-    <Router>
-      <Navbar/>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/properties">
-          <PropertiesList />
-        </Route>
-        <Route exact path="/test">
-          <TEST />
-        </Route>
-      </Switch>
-     </Router>
-=======
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "./components/contexts/AuthContext";
 import PrivateRoute from "./components/Authentication/PrivateRoute";
@@ -50,7 +21,6 @@ function App() {
         </Router>
       </AuthProvider>
     </>
->>>>>>> main
   );
 }
 
