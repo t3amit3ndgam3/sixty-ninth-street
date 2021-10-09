cc8dbb30f032cfdc25ab525783b8eb3dd9c7b7bd
GolamRabby96 <golamrabbynwu@gmail.com>
2021-10-10 01:15:12 +0600

    done

--- src/components/AddProperty/AddProperty.js
+++ src/components/AddProperty/AddProperty.js
@@ -1,273 +1,390 @@
-import React from "react";
-import "../Home/Unmoy.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 const AddProperty = () => {
+	const [formInfo, setFormInfo] = useState({});
+	const [specification, setSpecification] = useState({});
+	const [formFile, setFormFile] = useState({});
+
+	console.log(specification);
+
+	const handleBlur =(e)=>	{
+		const newData = {...formInfo};
+		newData[e.target.name ]= e.target.value
+		setFormInfo(newData);
+	}
+	const handleBlurSpecification =(e)=>	{
+		const newData = {...specification};
+		newData[e.target.name ]= e.target.value
+		setSpecification(newData);
+	}
+
+
+	const handleFileChange =(e)=>{
+		const newFile = {...formFile};
+		newFile[e.target.name] = e.target.files[0];
+		setFormFile(newFile);
+	}
+
+	const handleSubmit =(e)=>{
+		let BrandName = formInfo.productBrand.replace(/\s/g, "");
+		let CategoryName = formInfo.productCategory.replace(/\s/g, "");
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append('category', CategoryName);
+		formData.append('brand', BrandName);
+		formData.append('name', formInfo.productName);
+		formData.append('price', formInfo.productPrice);
+		formData.append('imageOne', formFile.imageOne);
+		formData.append('imageTwo', formFile.imageTwo);
+		formData.append('imageThree', formFile.imageThree);
+		formData.append('criteria1', specification.criteria1);
+		formData.append('criteria2', specification.criteria2);
+		formData.append('criteria3', specification.criteria3);
+		formData.append('criteria4', specification.criteria4);
+		formData.append('criteria5', specification.criteria5);
+		formData.append('criteria6', specification.criteria6);
+		formData.append('criteria7', specification.criteria7);
+		formData.append('criteria8', specification.criteria8);
+		formData.append('criteria9', specification.criteria9);
+		formData.append('criteria10', specification.criteria10);
+		formData.append('criteria11', specification.criteria11);
+		formData.append('criteria12', specification.criteria12);
+		formData.append('criteria13', specification.criteria13);
+		formData.append('criteria14', specification.criteria14);
+		formData.append('criteria15', specification.criteria15);
+		formData.append('criteria16', specification.criteria16);
+		formData.append('criteria17', specification.criteria17);
+
+		console.log(formData);
+		// fetch('http://localhost:5000/addProduct',{
+		// 	method:'POST',
+		// 	body:formData
+		// })
+		// .then( res => res.json())
+		// .then( data => {
+		// 	window.alert('Data Inserted');
+		// })
+		// .catch(error=> console.error(error))
+
+		// e.target.reset();
+	}
 	return (
-		<div className="addProperty_section navSpace">
-			<div className="addProperty_wrapper">
-				<h1>Add Property</h1>
-				<div>
-					<form>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Property for:</h6>
-							</div>
-							<div className="ckeckbox_wrapper">
-								<div className="input_wrapper">
-									<input
-										class="form-check-input"
-										type="radio"
-										name="PropertyFor"
-										id="flexRadioDefault1"
-									/>
-									<label class="form-check-label" for="flexRadioDefault1">
-										Sell
+		<div className="container-fluid">
+			<div className="row">
+				<div className="sidenavBar col-md-3">
+					<div className="DexSideNacBar">
+						<ul>
+							<li>
+								{" "}
+								<Link to="#">Add Product</Link>{" "}
+							</li>
+							<li>
+								{" "}
+								<Link to="#">All Product</Link>{" "}
+							</li>
+							<li>
+								{" "}
+								<Link to="#">Sells Product</Link>{" "}
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className="col-md-9 p-5">
+					<div className="addProductSection">
+						<form onSubmit={handleSubmit}>
+                            <div className="row">
+							<div className="col-md-6">
+								<div class="mb-2">
+									<label for="productCategory" class="form-label">
+										Product Category
 									</label>
-								</div>
-								<div className="input_wrapper">
 									<input
-										class="form-check-input"
-										type="radio"
-										name="PropertyFor"
-										id="flexRadioDefault2"
-										checked
+										type="text"
+										class="form-control"
+										id="productCategory"
+										name="productCategory"
+										required
+										onBlur={handleBlur}
 									/>
-									<label class="form-check-label" for="flexRadioDefault2">
-										Rent
+								</div>
+								<div class="mb-2">
+									<label for="productBrand" class="form-label">
+										Product Brand
 									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productBrand"
+										name="productBrand"
+										required
+										onBlur={handleBlur}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productName" class="form-label">
+										Product Name
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productName"
+										name="productName"
+										required
+										onBlur={handleBlur}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productPrice" class="form-label">
+										Product Price
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productPrice"
+										name="productPrice"
+										required
+										onBlur={handleBlur}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria1
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria1"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria2
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria2"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria3
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria3"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria4
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria4"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria5
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria5"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria6
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria6"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria7
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria7"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria8
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria8"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+
+							</div>
+							<div className="col-md-6">
+								<div class="mb-2">
+									<label for="imageOne" class="form-label">
+										Product Image One
+									</label>
+									<input class="form-control" type="file" id="imageOne"  name="imageOne" onChange={handleFileChange}/>
+								</div>
+								<div class="mb-2">
+									<label for="imageTwo" class="form-label">
+										Product Image two
+									</label>
+									<input class="form-control" type="file" id="imageTwo"  name="imageTwo" onChange={handleFileChange} />
+								</div>
+								<div class="mb-2">
+									<label for="imageThree" class="form-label">
+										Product Image three
+									</label>
+									<input class="form-control" type="file" id="imageThree"  name="imageThree" onChange={handleFileChange}/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria9
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria9"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria10
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria10"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria11
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria11"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria12
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria12"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria13
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria13"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria14
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria14"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria15
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria15"
+										onBlur={handleBlur}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria16
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria16"
+										onBlur={handleBlurSpecification}
+									/>
+								</div>
+								<div class="mb-2">
+									<label for="productDetails" class="form-label">
+									criteria17
+									</label>
+									<input
+										type="text"
+										class="form-control"
+										id="productDetails"
+										name="criteria17"
+										onBlur={handleBlurSpecification}
+									/>
 								</div>
 							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Property Type:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_wrapper">
-									<select name="" id="" className="select_box">
-										<option value="flat">Apartment/Flat</option>
-										<option value="duplex">Duplex Home</option>
-										<option value="showroom">Showroom</option>
-										<option value="office">Office Space</option>
-										<option value="shop">Shop</option>
-									</select>
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>City:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_wrapper">
-									<select name="" id="" className="select_box">
-										<option value="dhaka">Dhaka</option>
-										<option value="chittagong">Chittagong</option>
-										<option value="khulna">Khulna</option>
-										<option value="rajshahi">Rajshahi</option>
-										<option value="coxbazar">Cox's Bazar</option>
-										<option value="sylhet">Sylhet</option>
-										<option value="gazipur">Gazipur</option>
-										<option value="mymensingh">Mymensingh</option>
-										<option value="rangpur">Rangpur</option>
-									</select>
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Address:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="text" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Property Name:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="text" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Title:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="text" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Property Description:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="text" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Facing:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="text" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Property Size in sft:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="number" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Price:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="number" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Price is:</h6>
-							</div>
-							<div className="ckeckbox_wrapper">
-								<div className="input_wrapper">
-									<input type="checkbox" name="fixed" required />
-									<label for="fixed">Fixed</label>
-								</div>
-								<div className="input_wrapper">
-									<input type="checkbox" name="negotiable" />
-									<label for="negotiable">Negotiable</label>
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Bedroom:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="number" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Garages:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="number" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Balconies:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="number" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Bathroom:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="number" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Featured Image:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="file" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Gallery Image:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="file" name="" multiple />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Floor Plan:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="file" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Owner Name:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="text" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Owner Number:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="text" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section">
-							<div className="label_wrapper">
-								<h6>Owner email:</h6>
-							</div>
-							<div className="input_box">
-								<div className="input_field_wrapper">
-									<input className="input_field" type="text" name="" />
-								</div>
-							</div>
-						</div>
-						<div className="input_section p-5">
-							<button type="submit" className="btn btn-info container">
+							<button type="submit" className="container btn btn-info btn-sm">
 								Done
 							</button>
-						</div>
-					</form>
+                            </div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
 };
+
+// export default Admin;
 
 export default AddProperty;
