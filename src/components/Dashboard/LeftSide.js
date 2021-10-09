b4e23ac64df69d981905cbc7eadbb1e65a810594
GolamRabby96 <golamrabbynwu@gmail.com>
2021-10-09 19:36:54 +0600

    nothing

--- src/components/Dashboard/LeftSide.js
+++ src/components/Dashboard/LeftSide.js
@@ -1,18 +1,10 @@
 import React from "react";
 // import Profile_avater from "../../images/Profile_avatar.png";
-<<<<<<< HEAD
-import Profileer from "../../images/ema.png";
-=======
->>>>>>> main
 function LeftSide() {
   return (
     <div>
       <div className="card custom-card-left">
-<<<<<<< HEAD
-        <img src={Profileer} className="card-img-top" alt="..." />
-=======
         <img src="/" className="card-img-top" alt="..." />
->>>>>>> main
         <div className="card-body custom-user-dash">
           <h5 className="card-title custom-user-title">User Name</h5>
           <p className="card-text">
